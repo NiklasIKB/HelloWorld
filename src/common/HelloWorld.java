0dea424d9d5eae4e9effff3c71cff74ef8e702bc
Fokfritt <niitler19998@hotmail.com>
2024-11-07 14:35:12 +0100

    Added a comment

--- src/common/HelloWorld.java
+++ src/common/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello people");
-        //EXTRA BRANCH FOR THE PEOPLE
+        // syns detta
     }
 }
